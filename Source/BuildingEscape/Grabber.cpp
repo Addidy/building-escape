a0bce8be4ab6e8bdca9327ba53776ac93ac3c8e5
Adam Davies <adamd94@hotmail.com>
2016-09-13 19:15:59 +0100

    L74 LineTraceSingleByObjectType()
    Used LineTraceSingleByObjectType to detect if there are any physics body
    actors in front of me and logged that info to the console.
    
    P.S. TIL A logic error in c++ will cause EU to crash rather than warn
    you something is wrong

--- Source/BuildingEscape/Grabber.cpp
+++ Source/BuildingEscape/Grabber.cpp
@@ -41,5 +41,18 @@ void UGrabber::TickComponent( float DeltaTime, ELevelTick TickType, FActorCompon
 	FVector LineTraceEnd = PlayerViewPointLocation + (PlayerViewPointRotation.Vector() * Reach);
 
 	DrawDebugLine(GetWorld(), PlayerViewPointLocation, LineTraceEnd, FColor(255, 0, 0), false, 0.f, 0.f, 10.f);
+
+	///Setup query parameters
+	FCollisionQueryParams TraceParameters(FName(TEXT("")), false, GetOwner());
+
+	///Line-trace (AKA ray-cast) out to reach distance
+	FHitResult Hit;
+	GetWorld()->LineTraceSingleByObjectType(OUT Hit, PlayerViewPointLocation, LineTraceEnd, FCollisionObjectQueryParams(ECollisionChannel::ECC_PhysicsBody), TraceParameters);
+
+	///See what we hit
+	AActor* ActorHit = Hit.GetActor();
+	if (ActorHit) {
+		UE_LOG(LogTemp, Warning, TEXT("Tracehit: %s"), *(ActorHit->GetName()));
+	}
 }
 
