2409ce95858bf8be2af809014d4cc7312801620b
Adam Davies <adamd94@hotmail.com>
2016-09-13 23:19:04 +0100

    L77 FindComponentByClass()
    Added physics handler to defaultpawn. Added grabber code to find the
    physics handle component on play

--- Source/BuildingEscape/Grabber.cpp
+++ Source/BuildingEscape/Grabber.cpp
@@ -23,7 +23,14 @@ void UGrabber::BeginPlay()
 	Super::BeginPlay();
 
 	UE_LOG(LogTemp, Warning, TEXT("Grabber reporting for duty!"));
-	
+
+	///Look for attached Physics Handle
+	PhysicsHandle = GetOwner()->FindComponentByClass<UPhysicsHandleComponent>();
+	if (PhysicsHandle) {
+		// Physics handle is found
+	} else {
+		UE_LOG(LogTemp, Error, TEXT("%s has no PhysicsHandle component"), *GetOwner()->GetName());
+	}
 }
 
 
